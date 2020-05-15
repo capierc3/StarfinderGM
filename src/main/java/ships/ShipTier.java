8afb54946d4d48396274ece3d15c4da63156ff9a
Chase <cpierce2113@gmail.com>
2020-05-15 12:37:22 -0400

    added scales and tiers

--- src/main/java/ships/ShipTier.java
+++ src/main/java/ships/ShipTier.java
@@ -1,16 +1,17 @@
 package ships;
 
+import java.util.ArrayList;
+
 public class ShipTier implements ShipComponent {
 
     private String tier;
     private String buildPoints;
     private String special;
 
-    public ShipTier(String line) {
-        String[] split = line.split("\\?");
-        tier = split[0];
-        buildPoints = split[1];
-        special = split[2];
+    public ShipTier(String[] values) {
+        tier = values[0];
+        buildPoints = values[1];
+        special = values[2];
     }
 
     @Override
@@ -33,5 +34,38 @@ public class ShipTier implements ShipComponent {
 
     public String getSpecial() {
         return special;
+    }
+
+    public static ArrayList<ShipTier> getTiers() {
+        String[][] values = {
+                {"1/4","25","—"},
+                {"1/3","30","—"},
+                {"1/2","40","—"},
+                {"1","55","—"},
+                {"2","75","—"},
+                {"3","95","—"},
+                {"4","115","HP increase"},
+                {"5","135","—"},
+                {"6","155","—"},
+                {"7","180","—"},
+                {"8","205","HP increase"},
+                {"9","230","—"},
+                {"10","270","—"},
+                {"11","310","—"},
+                {"12","350","HP increase"},
+                {"13","400","—"},
+                {"14","450","—"},
+                {"15","500","—"},
+                {"16","600","HP increase"},
+                {"17","700","—"},
+                {"18","800","—"},
+                {"19","900","—"},
+                {"20","1,000","HP increase"},
+        };
+        ArrayList<ShipTier> tiers = new ArrayList<>();
+        for (String[] valueList : values) {
+            tiers.add(new ShipTier(valueList));
+        }
+        return tiers;
     }
 }
